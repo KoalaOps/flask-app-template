c8fd275649234ed3943fb0379789ffe6561d754c
Oded Regev <odedre@gmail.com>
2023-01-23 19:38:21 -0800

    /koala endpoint with dynamic values. scripts dir is out.

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,4 @@
+import toml
 from app import app
 
 @app.route('/')
@@ -7,4 +8,14 @@ def index():
 
 @app.route('/koala')
 def koala():
-	return "Hello world! (%s v%s)" % (app.config['SERVICE_NAME'], app.config['VERSION'])
+	service_name = ""
+	version = ""
+	with open(".koala.toml", "rb") as f:
+		toml_str = f.read().decode("utf-8")
+		parsed_toml = toml.loads(toml_str)
+		service_name = parsed_toml["Name"]
+	
+	with open("VERSION", "rb") as f:
+		version = f.read().decode("utf-8")
+
+	return "Hello Koala! (%s v%s)" % (service_name, version)
