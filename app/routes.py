ab386975ca93e9cbda9b1c59bd677cc5b1f46e94
Oded Regev <odedre@gmail.com>
2023-02-19 21:12:48 +0200

    add description

--- app/routes.py
+++ app/routes.py
@@ -9,13 +9,15 @@ def index():
 @app.route('/koala')
 def koala():
 	service_name = ""
+	description = ""
 	version = ""
 	with open(".koala.toml", "rb") as f:
 		toml_str = f.read().decode("utf-8")
 		parsed_toml = toml.loads(toml_str)
 		service_name = parsed_toml["Name"]
+		description = parsed_toml["Description"]
 	
 	with open("VERSION", "rb") as f:
 		version = f.read().decode("utf-8")
 
-	return "Hello Koala! (%s v%s)" % (service_name, version)
+	return "Hello Koala! (%s v%s) - %s" % (service_name, version, description)
